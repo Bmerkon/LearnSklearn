6f23787be6ca752e2f42246e507ee8f33dbdb208
Bmerkon <pizza1989521.ok@gmail.com>
2018-05-02 21:40:03 +0800

    增加两个常用transformer

--- PipelineBlocks/transformation.py
+++ PipelineBlocks/transformation.py
@@ -1,18 +1,15 @@
 from sklearn.base import TransformerMixin, BaseEstimator
-
+import pandas as pd
 
 class FactorExtractor(TransformerMixin,BaseEstimator):
     def __init__(self,factor):
-        if factor is not None:
-            self.factor=factor
-        else:
-            self.factor=None
+   
+        self.factor=factor
+        
     
     def transform(self, data):
-        if self.factor is None:
-            return data
-        else:
-            return data[self.factor]
+
+        return data[self.factor]
 
     def fit(self,*_):
         return self
@@ -20,3 +17,16 @@ class FactorExtractor(TransformerMixin,BaseEstimator):
 
 
 
+class ModelTransformer(TransformerMixin, BaseEstimator):
+    def __init__(self, name,model,eval_metric=None):
+        self.name=name
+        self.model=model
+        self.eval_metric=eval_metric
+    
+    def fit(self,*args,**kwargs):
+        self.model.fit(eval_metric=self.eval_metric,*args,**kwargs)
+        return self
+
+    def transform(self, X,**transform_params):
+        return pd.DataFrame(self.model.predict(X),columns=[self.name+'_prediction'])
+        
